83253573c510516352dbd8e67a949160b32f5de8
akshit2941 <sainiakshit2941@gmail.com>
2025-05-04 21:05:12 +0530

    lombok added

--- src/main/java/com/akshit/journalApp/entity/JournalEntry.java
+++ src/main/java/com/akshit/journalApp/entity/JournalEntry.java
@@ -6,19 +6,14 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
 @Document("journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
     private ObjectId id;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
     private String title;
 
@@ -26,27 +21,4 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
